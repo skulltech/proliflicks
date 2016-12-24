fecc77b98ddc99fee0c69325b0b4556e224f945f
SkullTech <sumit.ghosh32@gmail.com>
2016-12-24 23:12:21 +0530

    Add function to scan a directory

--- core.py
+++ core.py
@@ -24,14 +24,17 @@ class Media:
 	def edit_info(self, infodict):
 		self.info.update(infodict)
 
+def scan_directory(directory):
+	media_containers = ['3g2', 'wmv', 'webm', 'mp4', 'avi', 'mp4a', 'mpeg', 'sub', 'mka', 'm4v', 'ts', 'mkv', 'ra', 'rm', 'wma', 'ass', 'mpg', 'ram', '3gp', 'ogv', 'mov', 'ogm', 'asf', 'divx', 'ogg', 'ssa', 'qt', 'idx', 'nfo', 'wav', 'flv', '3gp2', 'iso', 'mk2', 'srt']
+	files = []
+	for (dirpath, dirnames, filenames) in os.walk(directory):
+		for file in filenames:
+			if (os.path.splitext(file)[1][1:] in media_containers):
+				files.append(os.path.join(dirpath, file))
 
-directory = input('Enter directory to scan: ')
-
-media_containers = ['3g2', 'wmv', 'webm', 'mp4', 'avi', 'mp4a', 'mpeg', 'sub', 'mka', 'm4v', 'ts', 'mkv', 'ra', 'rm', 'wma', 'ass', 'mpg', 'ram', '3gp', 'ogv', 'mov', 'ogm', 'asf', 'divx', 'ogg', 'ssa', 'qt', 'idx', 'nfo', 'wav', 'flv', '3gp2', 'iso', 'mk2', 'srt']
-files = []
-for (dirpath, dirnames, filenames) in os.walk(directory):
+	medias = []
 	for file in files:
-		if (os.path.splittext(file)[1] in media_containers):
-			files.append(file)
+		media = Media(file)
+		medias.append(media)
 
-print(files)
+	return medias
