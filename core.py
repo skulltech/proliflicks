acdc66624e6d87cb01e26de30f77eb835f0c8449
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-15 09:25:09 +0530

    Add some untracked files

--- core.py
+++ core.py
@@ -5,10 +5,12 @@ import hashlib
 
 import tmdbsimple as tmdb
 from guessit import guessit
-from pythonopensubtitles.opensubtitles import OpenSubtitles
-from pythonopensubtitles.utils import File
+# from pythonopensubtitles.opensubtitles import OpenSubtitles
+# from pythonopensubtitles.utils import File
 
-with open('credentials.json', 'r'):
+from utils import File
+
+with open('credentials.json', 'r') as f:
 	creds = json.load(f)
 
 EMAIL = creds['opensubtitles']['email']
@@ -42,45 +44,74 @@ class Media:
 		vid_containers = ['3g2', 'wmv', 'webm', 'mp4', 'avi', 'mp4a', 'mpeg', 'mka', 'm4v', 'ts', 'mkv', 'ra', 'rm', 'wma', 'ass', 'mpg', 'ram', '3gp', 'ogv', 'mov', 'ogm', 'asf', 'divx', 'ogg', 'ssa', 'qt', 'idx', 'nfo', 'wav', 'flv', '3gp2', 'iso', 'mk2']
 		sub_containers = ['sub', 'srt']
 
-		file_extension = os.path.splittext(self.filename)[1][1:]
+
+
+		file_extension = os.path.splitext(self.filename)[1][1:]
 		if file_extension in vid_containers:
 			return 'vid'
-		else if file_extension in sub_containers:
+		elif file_extension in sub_containers:
 			return 'sub'
 
 
-def scan_directory(directory):
-	media_containers = ['3g2', 'wmv', 'webm', 'mp4', 'avi', 'mp4a', 'mpeg', 'mka', 'm4v', 'ts', 'mkv', 'ra', 'rm', 'wma', 'ass', 'mpg', 'ram', '3gp', 'ogv', 'mov', 'ogm', 'asf', 'divx', 'ogg', 'ssa', 'qt', 'idx', 'nfo', 'wav', 'flv', '3gp2', 'iso', 'mk2', 'sub', 'srt']	
-	files = []
-	for (dirpath, dirnames, filenames) in os.walk(directory):
-		for file in filenames:
-			if (os.path.splitext(file)[1][1:] in media_containers):
-				files.append(os.path.join(dirpath, file))
+class Proliflicks:
+	def __init__(self, shelf):
+		self.shelf = shelf
 
-	medias = []
-	for file in files:
-		media = Media(file)
-		medias.append(media)
+	def add_directory(self, directory):
+		medias = self.scan_directory(directory)
+		for media in medias:
+			self.update_shelf(media)
 
-	return medias
+	def update_shelf(self, media):
+		hash = self.get_hash(media.abspath)
+		shelf[hash] = media
+
+	def scan_directory(self, directory):
+		media_containers = ['3g2', 'wmv', 'webm', 'mp4', 'avi', 'mp4a', 'mpeg', 'mka', 'm4v', 'ts', 'mkv', 'ra', 'rm', 'wma', 'ass', 'mpg', 'ram', '3gp', 'ogv', 'mov', 'ogm', 'asf', 'divx', 'ogg', 'ssa', 'qt', 'idx', 'nfo', 'wav', 'flv', '3gp2', 'iso', 'mk2', 'sub', 'srt']	
+		files = []
+		for (dirpath, dirnames, filenames) in os.walk(directory):
+			for file in filenames:
+				if (os.path.splitext(file)[1][1:] in media_containers):
+					files.append(os.path.join(dirpath, file))
+
+		medias = []
+		for file in files:
+			media = Media(file)
+			medias.append(media)
+
+		return medias
+
+	def fetch_subtitles(self, media):
+		os = OpenSubtitles()
+
+		token = os.login(EMAIL, PASSWORD)
+		if not type(token) == str:
+			raise Exception('[*] Bad OpenSubtitles credentials!')
+
+		f = File(media.abspath)
+		hash = f.get_hash()
+		size = f.size()
+
+		data = os.search_subtitles([{'sublanguageid': 'all', 'moviehash': hash, 'moviebytesize': size}])
+		return data
+
+	def get_hash(self, file_path):
+	    '''Return the md5 hash of a file.
+	    '''
+
+	    file = File(file_path)
+	    hash = file.get_hash()
+	    return hash
+
+	    # with open(file_path, 'rb') as file:
+	    # 	hash = hashlib.md5(file.read()).hexdigest()
+	    # return hash
 
 
-def fetch_subtitles(media):
-	os = OpenSubtitles()
 
-	token = os.login(EMAIL, PASSWORD)
-	if not type(token) == str:
-		raise Exception('[*] Bad OpenSubtitles credentials!')
+with shelve.open('spam') as shelf:
+	pf = Proliflicks(shelf)
+	pf.add_directory("E:\Movies")
 
-	f = File(media.abspath)
-	hash = f.get_hash()
-	size = f.size()
-
-	data = os.search_subtitles([{'sublanguageid': 'all', 'moviehash': hash, 'moviebytesize': size}])
-	return data
-
-def get_hash(file_path):
-    '''Return the md5 hash of a file.
-    '''
-    f = open(file_path, 'rb').read()
-    return hashlib.md5(f).hexdigest()
+	for (key, data) in shelf.items():
+		print(key, ': ', data.info['title'])
